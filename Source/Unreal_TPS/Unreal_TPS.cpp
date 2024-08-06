765ce1b39cd7e152f6f2dba276eba4db2f5d214b
아기새 <liebespaar93@gmail.com>
2024-08-06 11:33:23 +0900

    default unreal

---
+++ Source/Unreal_TPS/Unreal_TPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal_TPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_TPS, "Unreal_TPS" );
